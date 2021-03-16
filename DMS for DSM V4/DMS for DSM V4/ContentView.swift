a8613e9a94ab050f9597c5c59ec0c3d49d2e3422
DohyunKIm <rlaehgus1229@gmail.com>
2021-03-16 14:58:11 +0900

    [Add] XcodeProject

---
+++ DMS for DSM V4/DMS for DSM V4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DMS for DSM V4
+//
+//  Created by DohyunKim on 2021/03/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DMS for DSM V4/DMS for DSM V4/DMS_for_DSM_V4App.swift
@@ -0,0 +1,17 @@
+//
+//  DMS_for_DSM_V4App.swift
+//  DMS for DSM V4
+//
+//  Created by DohyunKim on 2021/03/16.
+//
+
+import SwiftUI
+
+@main
+struct DMS_for_DSM_V4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
