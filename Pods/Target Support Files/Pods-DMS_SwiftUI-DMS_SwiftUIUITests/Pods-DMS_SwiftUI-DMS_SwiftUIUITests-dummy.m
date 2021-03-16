f89fb240e1ee0d9787f7eb0412b6df7a3ff58a50
DohyunKIm <rlaehgus1229@gmail.com>
2021-03-16 14:52:17 +0900

    [Modify] file path

--- Pods/Target Support Files/Pods-DMS_SwiftUI-DMS_SwiftUIUITests/Pods-DMS_SwiftUI-DMS_SwiftUIUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DMS_SwiftUI_DMS_SwiftUIUITests : NSObject
-@end
-@implementation PodsDummy_Pods_DMS_SwiftUI_DMS_SwiftUIUITests
-@end
--- Pods/Target Support Files/Pods-DMS_SwiftUITests/Pods-DMS_SwiftUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DMS_SwiftUITests : NSObject
-@end
-@implementation PodsDummy_Pods_DMS_SwiftUITests
-@end
